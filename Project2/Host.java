2e8f1eee5f570210e99b8f10bfacb725223c5624
spruitni <spruitni@mail.gvsu.edu>
2017-10-30 21:44:42 -0400

    Add json file creation

--- Project2/Host.java
+++ Project2/Host.java
@@ -3,6 +3,8 @@ import java.net.*;
 import java.util.*;
 import java.text.*;
 import java.lang.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
 
 //Class performs host functions
 public class Host{
@@ -11,6 +13,7 @@ public class Host{
     private final String EOF = "EOF";
     private Socket socket;
     private DataOutputStream outToServer;
+    private final String JSON_FILE_NAME = "fileInfo.json";
 
     //Create a connection to server
     public void connectToServer(String serverIP, int serverPort){
@@ -33,7 +36,32 @@ public class Host{
             outToServer.close();
         }
         catch(IOException ex){
-            System.out.println("Problem writing to server");
+            System.out.println("Problem writing to server" + ex);
         }
+    }
+
+    //Create a JSON file made of three fields
+    public void uploadFile(String userName, String hostName, String connSpeed){
+        JSONObject obj = new JSONObject();
+        obj.put("userName", userName);
+        obj.put("num", hostName);
+        obj.put("balance", connSpeed);
+        try{
+            FileWriter file = new FileWriter(JSON_FILE_NAME);
+            file.write(obj.toJSONString());
+            file.close();
+        }
+        catch(IOException ex){
+            System.out.println("Problem writing JSON file: " + ex);
+        }
+    }    
+
+    /////////
+    //TESTING
+    /////////
+    public static void main(String args[]){
+        Host h = new Host();
+        h.uploadFile("userName", "127.0.0.1", "Ethernet");
+
     }
 }
